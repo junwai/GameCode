27f5a0d7dce6f9e6d3a0e20e012dc87671537e41
junwai <bgoolsby832@yahoo.com>
2020-03-30 16:53:58 -0400

    Add files via upload

---
+++ UnitV3.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 22:04:10 2020
+
+@author: bgool
+"""
+
+import collections
+
+class Player:
+    def __init__(self,unitClass,playerID):
+        self.unitClass = unitClass
+        self.level = 1
+        self.playerID = playerID
+        
+    def levelUP(self,newLevel):
+        self.level = newLevel
+
+    def getLevel(self):
+        return self.level
+    
+class LevelManager:
+    def __init__(self,level,unitClass,unitType):
+        self.unitClass = unitClass
+        self.level = level
+        self.unitType = unitType
+        self.unitAttributes = self.classAttributes()
+        
+    def setAttributes(self,attributes):
+        return dict(zip(['Health','Attack','Movement','Special','Reaction','Damage','Evasion','Hit','Armor'],attributes))
+
+    def getAttributes(self):
+        return self.unitAttributes
+    
+    def classAttributes(self):
+        classSwitch = {
+            'Warrior': self.WarriorAttributes(),
+            'Assassin': self.AssassinAttributes(),
+            'Mage': self.MageAttributes(),
+            'Engineer': self.EngineerAttributes()
+        }
+        return classSwitch.get(self.unitClass)
+    
+    def WarriorAttributes(self):
+        levelSwitchE = {
+            1: self.setAttributes([5,1,3,1,1,2,0,0,0]),
+            2: self.setAttributes([5,1,3,1,1,2,0,0,0]),
+            3: self.setAttributes([7,2,3,1,2,2,0,1,0]),
+            4: self.setAttributes([7,2,3,1,2,2,1,1,0]),
+            5: self.setAttributes([7,3,3,1,3,3,1,1,1]),
+            6: self.setAttributes([9,3,3,1,3,3,1,2,1]),
+            7: self.setAttributes([9,4,3,2,3,3,2,2,1]),
+            8: self.setAttributes([11,4,3,2,3,3,2,2,1]),
+            9: self.setAttributes([11,4,3,2,3,3,2,2,1]),
+            10: self.setAttributes([13,4,3,2,4,4,2,3,2])
+        }
+        levelSwitchC = {
+            1: self.setAttributes([3,1,3,1,1,2,0,0,0]),
+            2: self.setAttributes([3,1,3,1,1,2,0,0,0]),
+            3: self.setAttributes([5,1,3,1,2,2,0,0,0]),
+            4: self.setAttributes([5,1,3,1,2,2,0,0,0]),
+            5: self.setAttributes([5,2,3,1,2,2,0,1,0]),
+            6: self.setAttributes([6,2,3,1,3,2,0,1,1]),
+            7: self.setAttributes([6,2,3,1,3,2,1,1,1]),
+            8: self.setAttributes([7,2,3,1,3,3,1,1,1]),
+            9: self.setAttributes([7,2,3,2,3,3,1,1,1]),
+            10: self.setAttributes([8,2,3,2,3,3,1,2,1])
+        }
+
+        typeSwitch = {
+            'Elite': levelSwitchE.get(self.level),
+            'Common': levelSwitchC.get(self.level)
+        }
+        return typeSwitch.get(self.unitType)
+
+    def AssassinAttributes(self):
+        levelSwitchE = {
+            1: self.setAttributes([4,1,3,1,2,1,1,0,0]),
+            2: self.setAttributes([4,1,3,1,2,1,1,0,0]),
+            3: self.setAttributes([5,1,3,1,2,2,1,0,0]),
+            4: self.setAttributes([5,1,3,1,3,2,2,0,0]),
+            5: self.setAttributes([7,1,4,1,3,3,2,0,0]),
+            6: self.setAttributes([7,1,4,1,3,3,2,0,0]),
+            7: self.setAttributes([8,2,4,1,4,4,3,0,0]),
+            8: self.setAttributes([8,2,4,1,4,4,3,0,0]),
+            9: self.setAttributes([8,2,4,1,4,5,3,0,0]),
+            10: self.setAttributes([10,2,4,1,5,5,4,2,2])
+        }
+        levelSwitchC = {
+            1: self.setAttributes([3,1,3,1,2,1,1,1,0]),
+            2: self.setAttributes([3,1,3,1,2,1,1,1,0]),
+            3: self.setAttributes([4,1,3,1,2,1,2,2,0]),
+            4: self.setAttributes([4,1,4,1,3,2,2,2,0]),
+            5: self.setAttributes([5,1,4,1,3,2,2,2,0]),
+            6: self.setAttributes([5,1,4,1,3,2,2,2,1]),
+            7: self.setAttributes([6,2,4,1,3,2,2,2,1]),
+            8: self.setAttributes([6,2,4,1,4,2,2,2,1]),
+            9: self.setAttributes([7,2,4,1,4,2,2,3,1]),
+            10: self.setAttributes([7,2,4,1,4,3,3,3,1])
+        }
+
+        typeSwitch = {
+            'Elite': levelSwitchE.get(self.level),
+            'Common': levelSwitchC.get(self.level)
+        }
+        return typeSwitch.get(self.unitType)
+
+    def MageAttributes(self):
+        levelSwitchE = {
+            1: self.setAttributes([4,1,3,3,1,1,0,0,0]),
+            2: self.setAttributes([4,1,3,3,1,1,0,0,0]),
+            3: self.setAttributes([5,1,3,3,2,1,0,0,0]),
+            4: self.setAttributes([5,1,3,3,2,1,0,1,1]),
+            5: self.setAttributes([5,1,3,4,3,1,1,1,1]),
+            6: self.setAttributes([7,1,3,4,3,1,1,1,1]),
+            7: self.setAttributes([7,1,3,4,4,1,1,1,2]),
+            8: self.setAttributes([8,1,3,4,4,1,2,1,2]),
+            9: self.setAttributes([8,1,3,5,5,1,2,1,2]),
+            10: self.setAttributes([10,1,3,5,5,1,3,1,2])
+        }
+        levelSwitchC = {
+            1: self.setAttributes([3,1,3,0,0,1,0,0,0]),
+            2: self.setAttributes([3,1,3,0,0,1,0,0,0]),
+            3: self.setAttributes([4,1,3,0,0,2,0,0,0]),
+            4: self.setAttributes([4,1,3,0,0,2,1,0,0]),
+            5: self.setAttributes([4,1,3,0,0,2,1,0,0]),
+            6: self.setAttributes([4,1,3,0,0,2,1,1,0]),
+            7: self.setAttributes([4,1,3,0,0,2,1,1,0]),
+            8: self.setAttributes([5,1,3,0,0,2,1,1,0]),
+            9: self.setAttributes([5,1,4,0,0,2,1,1,0]),
+            10: self.setAttributes([5,1,4,0,0,2,2,1,0])
+        }
+
+        typeSwitch = {
+            'Elite': levelSwitchE.get(self.level),
+            'Common': levelSwitchC.get(self.level)
+        }
+        return typeSwitch.get(self.unitType)
+
+    def EngineerAttributes(self):
+        levelSwitchE = {
+            1: self.setAttributes([4,1,2,2,1,1,-2,-2,0]),
+            2: self.setAttributes([4,1,2,2,1,1,-2,-2,0]),
+            3: self.setAttributes([4,1,2,2,1,1,-2,-2,0]),
+            4: self.setAttributes([5,1,2,2,1,1,-2,-2,0]),
+            5: self.setAttributes([5,1,2,2,1,1,-2,-2,0]),
+            6: self.setAttributes([5,1,2,2,1,1,-2,-2,0]),
+            7: self.setAttributes([6,1,2,2,1,1,-2,-2,0]),
+            8: self.setAttributes([6,1,2,2,1,1,-2,-2,0]),
+            9: self.setAttributes([6,1,2,2,1,1,-2,-2,0]),
+            10: self.setAttributes([7,1,2,2,1,1,-2,-2,0])
+        }
+        levelSwitchC = {
+            1: self.setAttributes([4,1,3,0,0,1,-1,0,0]),
+            2: self.setAttributes([4,1,3,0,0,1,-1,1,0]),
+            3: self.setAttributes([5,2,3,0,0,2,-1,1,0]),
+            4: self.setAttributes([5,2,4,0,0,2,-1,1,1]),
+            5: self.setAttributes([5,2,4,0,0,2,0,2,1]),
+            6: self.setAttributes([6,2,4,0,0,2,0,2,1]),
+            7: self.setAttributes([6,2,4,0,0,2,0,2,2]),
+            8: self.setAttributes([6,2,5,0,0,2,0,2,2]),
+            9: self.setAttributes([7,2,5,0,0,3,0,2,2]),
+            10: self.setAttributes([7,3,5,0,0,3,0,2,3])
+        }
+        
+        typeSwitch = {
+            'Elite': levelSwitchE.get(self.level),
+            'Common': levelSwitchC.get(self.level)
+        }
+        return typeSwitch.get(self.unitType)
+       
+class AttributeManager():
+    def __init__(self,currentAttributes):
+        self.currentAttributes = currentAttributes
+    
+    def getAttributes(self,attribute):
+        return self.currentAttributes.get(attribute)
+    
+    def changeAttributes(self,attribute,value):
+        self.currentAttributes[attribute] = self.currentAttributes[attribute] + value
+        
+class Unit():
+    def __init__(self,metadata,location,direction,players):
+        self.metadata = metadata
+        # metadata includes playerID, unitClass, unitType, number, board location, and directionality
+        self.unitID = metadata.playerID+'_'+metadata.unitType+str(metadata.number)
+        self.location = location
+        self.direction = direction
+        self.level = self.getPlayerLevel(players)[0]
+        self.levelManager = LevelManager(self.level,metadata.unitClass,metadata.unitType)
+        self.unitAttributes = self.levelManager.getAttributes()
+        self.attributeManager = AttributeManager(self.unitAttributes)
+        
+        self.getPlayerLevel(players)
+        
+    def getPlayerLevel(self,players):
+        return [x.level for x in players if x.playerID == self.metadata.playerID]
+        
+    def adjacentSquares(self):
+        #[1,2,3,4,5,6]
+        x = self.location[0]
+        y = self.location[1]
+        switch = {
+            'n': {1:(x,y+1),2:(x+1,y+1),3:(x+1,y),4:(x,y-1),5:(x-1,y-1),6:(x-1,y)},
+            'ne': {1:(x+1,y+1),2:(x+1,y),3:(x,y-1),4:(x-1,y-1),5:(x-1,y),6:(x,y+1)},
+            'se': {1:(x-1,y-1),2:(x-1,y),3:(x,y+1),4:(x+1,y+1),5:(x+1,y),6:(x,y-1)},
+            's': {1:(x,y-1),2:(x-1,y-1),3:(x-1,y),4:(x,y+1),5:(x+1,y+1),6:(x+1,y)},
+            'sw': {1:(x+1,y),2:(x,y-1),3:(x-1,y-1),4:(x-1,y),5:(x,y+1),6:(x+1,y+1)},
+            'nw': {1:(x-1,y),2:(x,y+1),3:(x+1,y+1),4:(x+1,y),5:(x,y-1),6:(x-1,y-1)}
+        }
+        switch.get(self.direction,'error')
+    
+    def move(self,gameboard,movedirection,*args):
+        gameboard[gameboard[self.location].adjacentSquares(self.location,self.direction)]
+        self.attributeManager.changeAttributes('Movement',-1)
+        
+    def attack(self,target,gameboard):
+        self.attributeManager.changeAttributes('Attack',-1)
+        
+    def special(self):
+        self.attributeManager.changeAttributes('Special',-1)
+        
+    def reaction(self):
+        self.attributeManager.changeAttributes('Reaction',-1)
+        
+class Game:
+    def __init__(self):
+        self.gameboard = {}
+        self._unit = collections.namedtuple('unit','playerID unitClass unitType number')
+        self.players = [Player('Warrior','Player1'), Player('Assassin','Player2')]
+        
+    def placeUnit(self,location,drn,players):
+        self.gameboard[location] = Unit(self._unit(playerID = 'Player1', unitClass= 'Warrior', unitType='Common', number = 1), location,drn,players)
+
+    def main(self):
+        self.gameboard[(0,0)] = Unit(self._unit(playerID = self.players[0].playerID, unitClass= 'Warrior', unitType='Common', number = 1),[0,0],'n',self.players)
+        self.placeUnit((0,0),'n',self.players)
+        
+Game().main()
